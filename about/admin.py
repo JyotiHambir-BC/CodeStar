76d5723acd33d807ecdb16197b3b7f38cf0bad6b
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-09-13 09:49:43 +0000

    create about app and updated admin and models file of this app

---
+++ about/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import About
+from django_summernote.admin import SummernoteInlineModelAdmin
+
+# Register your models here.
+
+@admin.register(About)
+class AboutAdmin(SummernoteInlineModelAdmin):
+    summernote_fields = ('content',)
